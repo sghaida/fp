f2fc971afb46e72850ccefc4ee782b8ccd81043a
saddam abu ghaida <saddam.abughaida@gmail.com>
2020-04-19 05:13:39 +0100

    add the template for either

--- src/Monads/either.go
+++ src/Monads/either.go
@@ -1,1 +1,33 @@
 package Monads
+
+type EitherType struct {
+	left  error
+	right struct{}
+}
+
+func Either() EitherType {
+	return EitherType{}
+}
+
+func (e *EitherType) IsLeft() bool {
+	panic("not implemented yet")
+}
+
+func (e *EitherType) IsRight() bool {
+	panic("not implemented yet")
+}
+
+func (e *EitherType) Left(err error) EitherType {
+	// TODO check right
+	panic("not implemented yet")
+}
+
+func (e *EitherType) Right(data struct{}) EitherType {
+	// TODO check left
+	panic("not implemented yet")
+}
+
+func (e *EitherType) Get() interface{} {
+	// TODO check left and Right
+	panic("not implemented yet")
+}
