6a11213c42f361a993d208ea370709f1093865c1
saddam abu ghaida <saddam.abughaida@gmail.com>
2020-04-19 23:20:07 +0100

    make it more readable

--- src/apply/apply.go
+++ src/apply/apply.go
@@ -27,39 +27,16 @@ func (t *Type) Apply(f interface{}) Type {
 		return t.applyString(s, f)
 	}
 	// make sure that this is a function
-	v := reflect.ValueOf(f)
+	fv := reflect.ValueOf(f)
 	// check if d is a function to start with
-	checkFuncKind(v)
+	checkFuncKind(fv)
 	// check input | output of the func
-	fType := v.Type()
+	fType := fv.Type()
 	// check function take one parameter of type t and return one out put of the same type
 	if fType.NumOut() != 1 || fType.NumIn() != 1 {
 		panic("Function must take one input and produces one output")
 	}
-	// get th inbound param type
-	inV := fType.In(0)
-	inKind := inV.Kind()
-	// get outbound param type
-	outV := fType.Out(0)
-	outKind := outV.Kind()
-	// check types for input and output
-	if inKind != outKind {
-		panic("function input type != output type")
-	}
-	if reflect.TypeOf(t.slice).Elem().Kind() != inKind {
-		panic("slice type != map function input type")
-	}
-	// create empty slice based on the original slice
-	origSlice := reflect.ValueOf(t.slice)
-	newSlice := make([]interface{}, origSlice.Len())
-	// apply the method then append the item to the new slice
-	var in [1]reflect.Value
-	for i := 0; i < origSlice.Len(); i++ {
-		in[0] = origSlice.Index(i)
-		newSlice[i] = v.Call(in[:])[0].Interface()
-	}
-	// return new mapped results
-	return Type{slice: newSlice}
+	return t.applyGeneric(fType, fv)
 }
 
 func (t *Type) Get() interface{} {
@@ -75,6 +52,33 @@ func (t *Type) applyString(s []string, f interface{}) Type {
 		return Type{slice: newSlice}
 	}
 	panic("function signature is not supported")
+}
+
+func (t *Type) applyGeneric(tp reflect.Type, fv reflect.Value) Type {
+	// get th inbound param type
+	inV := tp.In(0)
+	inKind := inV.Kind()
+	// get outbound param type
+	outV := tp.Out(0)
+	outKind := outV.Kind()
+	// check types for input and output
+	if inKind != outKind {
+		panic("function input type != output type")
+	}
+	if reflect.TypeOf(t.slice).Elem().Kind() != inKind {
+		panic("slice type != map function input type")
+	}
+	// create empty slice based on the original slice
+	origSlice := reflect.ValueOf(t.slice)
+	newSlice := make([]interface{}, origSlice.Len())
+	// apply the method then append the item to the new slice
+	var in [1]reflect.Value
+	for i := 0; i < origSlice.Len(); i++ {
+		in[0] = origSlice.Index(i)
+		newSlice[i] = fv.Call(in[:])[0].Interface()
+	}
+	// return new mapped results
+	return Type{slice: newSlice}
 }
 
 func checkFuncKind(v reflect.Value) {
